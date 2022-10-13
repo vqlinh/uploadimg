c4feae0e5f3aea61a011d922a158876130212414
unknown <vuongquanglinh1234@gmail.com>
2022-10-14 00:24:59 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+var upload = document.querySelector("#mypicture")
+var preview = document.querySelector(".preview")
+var error = document.querySelector(".error")
+
+
+
+upload.addEventListener("change", function(e) {
+
+
+    var file = upload.files[0];
+
+    if(!file)
+        return;
+
+    if(!file.name.endsWith(".jpg")){
+        error.innerHTML = `hinh anh phai thuoc dinh dang jpeg`
+        return;
+
+    }
+    else{
+        error.innerHTML = ``
+    }
+
+    if(!file.size / (1024*1024) >5 ){
+        error.innerHTML = `hinh anh phai <5MB`
+        return;
+
+    }
+    else{
+        error.innerHTML = ``
+    }
+
+    var img = document.createElement("img")
+    img.src = URL.createObjectURL(file)
+    preview.appendChild(img)
+
+
+})
